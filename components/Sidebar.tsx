a2befd58c83d98f044367fe6dab0f18dd77e2b67
Yonatan Magier <31913495+yonatanmgr@users.noreply.github.com>
2025-01-28 02:54:30 +0200

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -35,7 +35,7 @@ const Sidebar = ({ selectedCourse, currentCourseGrades }: SidebarProps) => {
   return (
     <section
       className={
-        "max-h-full sm:min-h-full sm:h-full max-sm:min-h-fit min-w-1/4 sm:overflow-y-hidden overflow-x-hidden p-2 flex flex-col gap-2 sm:w-1/4 w-full min-w-[300px] rounded-lg bg-zinc-100 dark:bg-zinc-900/30 dark:border-zinc-500/20 border"
+        "max-h-full sm:min-h-full sm:h-full max-sm:min-h-fit min-w-1/4 sm:overflow-y-hidden overflow-x-hidden p-2 flex flex-col gap-2 sm:w-1/4 w-full min-w-[300px] rounded-xl bg-zinc-100 dark:bg-zinc-900/30 dark:border-zinc-500/20 border"
       }
     >
       {selectedCourse &&
